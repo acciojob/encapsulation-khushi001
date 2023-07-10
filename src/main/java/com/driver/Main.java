60b71818fd1ed248291f9cb478f347436855defb
khushi001 <khushiagarwal2501@gmail.com>
2023-07-11 00:09:17 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        // obj.name = "John"; // Error: The field name is not visible
+        // System.out.println(obj.name); // Error: The field name is not visible
+
+        obj.setName("John");
+        System.out.println(obj.getName());
+    }
 }
